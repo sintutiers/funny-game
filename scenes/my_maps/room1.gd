96cbf3a699a96bddb44f8c2cb386b608222712ba
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-06-10 22:39:03 +0200

    web has stricter memory/threading reqq
    .

--- scenes/my_maps/room1.gd
+++ scenes/my_maps/room1.gd
@@ -8,10 +8,13 @@ extends Node2D
 
 
 func _ready() -> void:
-	if not door.interacted_static.is_connected(_on_door_interacted_static):
-		var _err_door: Error = door.interacted_static.connect(_on_door_interacted_static) as Error
-	if not bed.interacted_static.is_connected(_on_bed_interacted_static):
-		var _err_bed: Error = bed.interacted_static.connect(_on_bed_interacted_static) as Error
+	door.interacted_static.connect(_on_door_interacted_static)
+	bed.interacted_static.connect(_on_bed_interacted_static)
+
+
+func _exit_tree() -> void:
+	door.interacted_static.disconnect(_on_door_interacted_static)
+	bed.interacted_static.disconnect(_on_bed_interacted_static)
 
 
 func _on_door_interacted_static(_body: RapierArea2D) -> void:
