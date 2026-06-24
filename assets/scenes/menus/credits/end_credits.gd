9eb2ec77cfdc4061b97727b754b8f8dfd2ce302b
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-06-24 20:02:15 +0200

    add cassie art and add nexus

--- assets/scenes/menus/credits/end_credits.gd
+++ assets/scenes/menus/credits/end_credits.gd
@@ -1,5 +1,5 @@
 @tool
-extends "res://scenes/menus/credits/scrolling_credits.gd"
+extends "res://assets/scenes/menus/credits/scrollable_credits.gd"
 
 ## Defines the path to the main menu. Hides the Main Menu button if not set.
 ## Will attempt to read from AppConfig if left empty.
